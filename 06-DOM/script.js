9a55385ace3ff4cc2293b456bc4ac3c2a16ded50
ZZ <zyz910420@gmail.com>
2024-07-02 17:30:02 +0800

    2024/7/2 practice

---
+++ 06-DOM/script.js
@@ -0,0 +1,11 @@
+const btn=document.getElementById("button")
+// getElementById => 運用<id>找元素
+// querySelector => 可運用<class>、<id>找元素，在括弧字串中<class>要用【.】/ <id>要用【#】
+const QueryBtn=document.querySelector("#button")
+
+btn.addEventListener("click", e=>{
+    const newBox=document.createElement("div")  // 在js裡創建存放<div>元素的變數空間
+    newBox.classList.add("box") // 將newBox歸類在"box"這個<class>，讓他可以吃到<style>的樣式
+    const container=document.querySelector(".container")    //找出可以存放newBox的父元素，而"container"為<class>，所以用querySelector(".")來找尋
+    container.appendChild(newBox)   // 父元素當中再加上其他的子元素
+})
